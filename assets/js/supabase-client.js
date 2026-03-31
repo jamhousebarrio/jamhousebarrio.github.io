aae01ef9d355e5b6b27610b82fd7dcb5b05313ae
Frank <fanteevi@gmail.com>
2026-03-31 18:54:42 +0200

    Fix Supabase anon key to correct JWT value

--- assets/js/supabase-client.js
+++ assets/js/supabase-client.js
@@ -4,7 +4,7 @@
   window.JH = window.JH || {};
 
   var SUPABASE_URL = 'https://gtyogyuwuwuxorchfhbo.supabase.co';
-  var SUPABASE_ANON_KEY = 'sb_publishable_J4iU0HzzkTR58-ffM7cGcA_poxz_KgJ';
+  var SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0eW9neXV3dXd1eG9yY2hmaGJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ5NTY2ODEsImV4cCI6MjA5MDUzMjY4MX0.NwaNImFF8zwQpMUdJIVBMSI61YuatRqQNcd6CIUwytI';
 
   if (typeof supabase !== 'undefined' && SUPABASE_URL && SUPABASE_ANON_KEY) {
     JH.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
